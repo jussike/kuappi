aa9cadee433886204ef7f66020924ba4295f6595
Jussi Keranen <jussike@gmail.com>
2020-01-18 15:52:30 +0200

    Refactor ValloxSerial

--- valloxcontrol.py
+++ valloxcontrol.py
@@ -25,26 +25,26 @@ class ValloxControl(AbstractControl):
 
 
 class ValloxSerial:
-    speeds = {
-              1: b'\x01\x22\x11\x29\x01\x5e',
-              2: b'\x01\x22\x11\x29\x03\x60',
-              3: b'\x01\x22\x11\x29\x07\x64',
-              4: b'\x01\x22\x11\x29\x0f\x6c',
-              5: b'\x01\x22\x11\x29\x1f\x7c',
-              6: b'\x01\x22\x11\x29\x3f\x9c',
-              7: b'\x01\x22\x11\x29\x7f\xdc',
-              8: b'\x01\x22\x11\x29\xff\x5c',
+    system = b'\x01'
+    sender = b'\x22'
+    destinations = {
+        'host': b'\x11',
+        'remote_broadcast': b'\x20',
     }
-
-    broadcast_speeds = {
-              1: b'\x01\x22\x20\x29\x01\x6d',
-              2: b'\x01\x22\x20\x29\x03\x6f',
-              3: b'\x01\x22\x20\x29\x07\x73',
-              4: b'\x01\x22\x20\x29\x0f\x7b',
-              5: b'\x01\x22\x20\x29\x1f\x8b',
-              6: b'\x01\x22\x20\x29\x3f\xab',
-              7: b'\x01\x22\x20\x29\x7f\xeb',
-              8: b'\x01\x22\x20\x29\xff\x6b',
+    commands = {
+        'speed': b'\x29',
+    }
+    values = {
+        'speed': {
+            1: b'\x01',
+            2: b'\x03',
+            3: b'\x07',
+            4: b'\x0f',
+            5: b'\x1f',
+            6: b'\x3f',
+            7: b'\x7f',
+            8: b'\xff',
+        }
     }
 
     def __init__(self, control):
@@ -52,8 +52,16 @@ class ValloxSerial:
         self.control = control
         self.checksum_byte = None
 
+    def get_control_data(self, destination, command, value):
+        control_data = self.system + self.sender + self.destinations[destination] + self.commands[command] + self.values[command][value]
+        checksum = 0
+        for byte in control_data:
+            checksum += byte
+        control_data += bytes((checksum,))
+        return control_data
+
     def set_speed(self, speed):
-        control_data = self.speeds[speed]
+        control_data = self.get_control_data('host', 'speed', speed)
         self.ser.write(control_data)
         self.listen_ack(control_data[-1], speed)
 
@@ -62,7 +70,7 @@ class ValloxSerial:
         listen_thread.start()
 
     def inform_remotes(self, speed):
-        control_data = self.broadcast_speeds[speed]
+        control_data = self.get_control_data('remote_broadcast', 'speed', speed)
         self.ser.write(control_data)
 
     def run(self, checksum, state):
@@ -77,22 +85,3 @@ class ValloxSerial:
                         self.inform_remotes(state)
                         return
             time.sleep(0.1)
-
-#echo -ne '\x01\x22\x10\x29\x07\x63' | cat >/dev/ttyUSB0
-#ed           01  11  22  08  01  3d
-#ser.write(b'\x01\x22\x11\x00\x71\xa5') # 01 11 22 71 e5 8a
-#ser.write(b'\x01\x22\x11\x00\x79\xad')
-# kysy I/O
-#ser.write(b'\x01\x22\x11\x00\x08\x3c') # 01112208013d
-#kysy huoltomuistutin
-#ser.write(b'\x01\x22\x11\x00\xa6\xda')
-#aseta huoltomuistutin
-#ser.write(b'\x01\x22\x11\xa6\x06\xe0')
-
-#ser.write(b'\x01\x22\x11\x00\xab\xdf')
-#ser.write(b'\x01\x22\x11\xab\x05\xe4')
-
-#ser.write(b'\x01\x22\x11\x08\x01\x3d')
-
-#ser.write(b'\x01\x22\x11\x08\x08\x44')
-#ser.write(b'\x01\x22\x11\x08\x28\x64')
