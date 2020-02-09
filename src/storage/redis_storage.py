746e887fed738fd20319e870499e206e2fa06419
Jussi Keranen <jussike@gmail.com>
2020-02-09 22:34:43 +0200

    Keep redis values up to 1 week

--- src/storage/redis_storage.py
+++ src/storage/redis_storage.py
@@ -10,17 +10,23 @@ class Redis:
         redis_host = "localhost"
         redis_port = 6379
         redis_password = ""
+        self.ttl = 60 * 60 * 24 * 7 * 1000 # 1 week as millis
         try:
             self.redis = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
         except Exception as e:
             logging.exception(e)
 
+    def remove_old_values(self, key, time):
+        self.redis.zremrangebyscore(key, 0, time - self.ttl)
+
     def add(self, value, key='temp'):
         c_time = current_millis()
         self.redis.zadd(key, {"%s:%s" % (c_time, value): c_time})
+        self.remove_old_values(key, c_time)
 
     def add_multi(self, values, keys=('temp', 'switch')):
         kv_dict = dict(zip(keys, values))
         c_time = current_millis()
         for key, value in kv_dict.items():
             self.redis.zadd(key, {"%s:%s" % (c_time, value): c_time})
+            self.remove_old_values(key, c_time)
