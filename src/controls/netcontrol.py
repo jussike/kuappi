98ec63b90cb6ddaa84e83de7af846198486115da
Jussi Keranen <jussi.keranen@vincit.com>
2021-11-06 17:10:35 +0200

    New NetControl

---
+++ src/controls/netcontrol.py
@@ -0,0 +1,24 @@
+import logging
+import socket
+
+from abstract import AbstractControl
+from config import CONFIG
+
+
+class NetControl(AbstractControl):
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.host = CONFIG.get('netctl_host')
+        self.port = CONFIG.get('netctl_udp_port')
+        self._state = None
+
+    def control(self, value):
+        self.socket.sendto(value, (self.host, self.port))
+        self._state = value
+
+    @property
+    def state(self):
+        return self._state
+
+    def cleanup(self):
+        self.socket.close()
