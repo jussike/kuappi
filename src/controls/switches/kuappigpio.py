ab2f9b3e6948cd445b5e6d369ff9a2aa2186e429
Jussi Keranen <jussike@gmail.com>
2021-11-24 22:13:20 +0200

    Change RPi.GPIO to gpiozero

--- src/controls/switches/kuappigpio.py
+++ src/controls/switches/kuappigpio.py
@@ -4,33 +4,33 @@ from config import CONFIG
 from abstract import AbstractSwitch
 
 if 'KuappiGPIO' in CONFIG.get('controls'):
-    import RPi.GPIO as GPIO
+    from gpiozero import LED as GPIO
 
 
 class KuappiGPIO(AbstractSwitch):
     def __init__(self):
         self.pin = CONFIG.get('gpio_pin')
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.pin, GPIO.OUT, initial=GPIO.HIGH)
+        self.gpio = GPIO(self.pin)
+        self.gpio.on()
         self._state = True
 
     def on(self):
         try:
-            GPIO.output(self.pin, True)
+            self.gpio.on()
             self._state = True
         except Exception:
             logging.exception('GPIO error when setting True')
 
     def off(self):
         try:
-            GPIO.output(self.pin, False)
+            self.gpio.off()
             self._state = False
         except Exception:
             logging.exception('GPIO error when setting False')
 
     def cleanup(self):
-        GPIO.cleanup(self.pin)
+        pass
 
     @property
     def state(self):
-        return GPIO.input(self.pin) and self._state
+        return self.gpio.value and self._state
