0a2a02336e7a2966ee4d027036f8509daf4a54a0
Jussi Keranen <jussike@gmail.com>
2021-11-24 23:49:03 +0200

    fridgealarm: Send alarm msg to bot

--- src/decisions/fridgealarmdecision.py
+++ src/decisions/fridgealarmdecision.py
@@ -10,15 +10,23 @@ class FridgeAlarmDecision(AbstractDecision):
     warn_limit = 10
 
     def __init__(self):
+        super().__init__(zmq=True)
         logging.info('Using FridgeAlarmDecision')
         self.cached_data_count = 0
         self.old_data = None
 
     def get_decision(self, data, output_state=None):
+        if self.remote_controlled:
+            logging.info('Snoozed')
+            if not self.remote_control_decision:
+                self.zmq_pub.normal()
+            return self.remote_control_decision
+
         if data == self.old_data:
             self.cached_data_count += 1
             if self.cached_data_count >= self.cached_limit:
                 logging.error('Data may be too old, raising alarm')
+                self.zmq_pub.send_alarm('Alarm: Old data!')
                 return True
             elif self.cached_data_count >= self.warn_limit:
                 logging.info('Data has been same than previous one %d times. There may be problem with the data source', self.cached_data_count)
@@ -29,5 +37,13 @@ class FridgeAlarmDecision(AbstractDecision):
         if not data or TEMP not in data.keys():
             return None
 
-        temp = data[TEMP]
-        return temp > self.hi_limit or temp < self.low_limit
+        if data[TEMP] > self.hi_limit or data[TEMP] < self.low_limit:
+            self.zmq_pub.send_alarm('Alarm: Temperature {} is outside of the limits ({} - {})'.format(data[TEMP], self.low_limit, self.hi_limit))
+            return True
+
+        self.zmq_pub.normal()
+        return False
+
+    def remote_snooze(self, **kwargs):
+        self.control()
+        self.zmq_pub.send('Snoozed for {} secs'.format(self.control_time))
