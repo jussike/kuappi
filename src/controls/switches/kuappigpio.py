074caab31daccbf44fe4084d75bd39b2a3d34928
Jussi Keranen <jussike@gmail.com>
2021-12-19 17:57:47 +0200

    Revert "Change RPi.GPIO to gpiozero"
    This reverts commit ab2f9b3e6948cd445b5e6d369ff9a2aa2186e429.

--- src/controls/switches/kuappigpio.py
+++ src/controls/switches/kuappigpio.py
@@ -4,33 +4,33 @@ from config import CONFIG
 from abstract import AbstractSwitch
 
 if 'KuappiGPIO' in CONFIG.get('controls'):
-    from gpiozero import LED as GPIO
+    import RPi.GPIO as GPIO
 
 
 class KuappiGPIO(AbstractSwitch):
     def __init__(self):
         self.pin = CONFIG.get('gpio_pin')
-        self.gpio = GPIO(self.pin)
-        self.gpio.on()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.OUT, initial=GPIO.HIGH)
         self._state = True
 
     def on(self):
         try:
-            self.gpio.on()
+            GPIO.output(self.pin, True)
             self._state = True
         except Exception:
             logging.exception('GPIO error when setting True')
 
     def off(self):
         try:
-            self.gpio.off()
+            GPIO.output(self.pin, False)
             self._state = False
         except Exception:
             logging.exception('GPIO error when setting False')
 
     def cleanup(self):
-        pass
+        GPIO.cleanup(self.pin)
 
     @property
     def state(self):
-        return self.gpio.value and self._state
+        return GPIO.input(self.pin) and self._state
